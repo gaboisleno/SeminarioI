28fa708da73e15fb0ad987a7f740439bec32f772
gaboisleno <isleno.gabriel@gmail.com>
2018-11-17 22:12:05 -0300

    CrystalReport

---
+++ Capa_Presentacion/MyReport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semi.Presentacion {
+    public partial class MyReport: Form {
+        public MyReport()
+        {
+            InitializeComponent();
+        }
+
+        private void MyReport_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
